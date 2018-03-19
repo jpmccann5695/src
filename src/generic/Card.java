2c311602cde4a6ff0a83a275ba7fc04ed20b3100
jpmccann5695 <30531377+jpmccann5695@users.noreply.github.com>
2018-03-19 15:30:02 -0500

    Changes made at PFG on 3/19
    Worked on MCH Rummy Gamepaly
    - Get Books
    - Started Get Runs

---
+++ src/generic/Card.java
@@ -0,0 +1,37 @@
+package generic;
+
+public class Card {
+	public String Suit;
+	public String Name;
+	public int Value, suitValue; //Diamond = 0, Clubs = 1, Hearts = 2, Spades = 3, Joker 4
+								
+
+	public Card(String Suit, String Name, int Value, int suitValue) {
+		this.Suit = Suit;
+		this.Name = Name;
+		this.Value = Value;
+		this.suitValue = suitValue;
+	}
+
+	@Override
+	public String toString() {
+		if (Value > 1 && Value < 11)
+			return Value + "" + Suit.charAt(0);
+		return Name.charAt(0) + "" + Suit.charAt(0);
+	}
+	public String getSuit(){
+		return this.Suit;
+	}
+	public boolean isSame(int value, int suitValue){
+		if(value == this.Value){
+			if(suitValue == this.suitValue)
+				return true;
+		}
+		return false;
+	}
+	public boolean nonSuitFinder(int value){
+		if(value == this.Value)
+			return true;
+		return false;
+	}
+}
---
+++ src/michRummy/Backend.java
@@ -0,0 +1,121 @@
+package michRummy;
+
+import java.util.ArrayList;
+import java.util.concurrent.ThreadLocalRandom;
+
+import generic.Deck;
+import generic.Hand;
+import generic.Player;
+
+public class Backend {
+
+	Deck deal, discard;
+	Player user, ai1, ai2,ai3;
+	int userScore, ai1Score, ai2Score, ai3Score, dealer,level;
+	boolean gameOver;
+	
+	void newGame(){
+		ai1 = new Player("AI West");
+		ai2 = new Player("AI North");
+		ai3 = new Player("Ai East");
+		user = new Player("You");
+		userScore = 0;
+		ai1Score = 0;
+		ai2Score = 0;
+		ai3Score = 0;
+		dealer = ThreadLocalRandom.current().nextInt(0, 4);		
+		level = 6;
+		gameOver = false;
+	}
+	
+	void playHand(){
+		boolean complete = false;
+		if(level >12){
+			gameOver = true;
+			return;
+		}
+		deal = new Deck('m');
+		deal.shuffle();
+		user.setHand(new Hand(level,deal));
+		ai1.setHand(new Hand(level,deal));
+		ai2.setHand(new Hand(level,deal));
+		ai3.setHand(new Hand(level,deal));
+		
+//		while(1) {
+//			
+//		}
+	}
+
+	int bookCount(Hand check) {
+		int count = 0;
+		int[] valueCount = check.valueCount();
+		if(valueCount[0]>0) {
+			int tempJoker = valueCount[0];
+			for(int i = 1; i< valueCount.length; i++) {
+				if(valueCount[i]>2) {
+					count++;
+				}
+				else if(valueCount[i]>1) {
+					if(tempJoker>0) {
+						tempJoker--;
+						count++;
+					}
+				}
+			}
+		}
+		else {
+			for(int i = 1; i< valueCount.length; i++) {
+				if(valueCount[i]>2)
+					count++;
+			}
+		}
+		return count;
+	}
+	int runCount(Hand check) {
+		int count = 0;
+		int[] valueCount = check.valueCount();
+		if(valueCount[0]>0) {
+			
+		}
+		else {
+			for(int i = 1; i< valueCount.length; i++) {
+				if(i==1) {
+					
+				}
+				if(valueCount[i] >1){
+					
+				}
+				else {
+					
+				}
+			}
+		}
+		return count;
+	}
+	boolean layableHand(Hand check) {
+		
+		
+		if(level == 6) {
+			
+		}
+		else if(level ==7) {
+			
+		}
+		else if(level == 8) {
+			
+		}
+		else if(level ==9) {
+			
+		}
+		else if(level == 10) {
+			
+		}
+		else if(level == 11) {
+			
+		}
+		else {
+			
+		}
+		return true;
+	}
+}
